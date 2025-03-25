98d8a0e760edbd070938ab8d311e0eba04713593
Devanshee-Gupta <154060860+Devanshee-Gupta@users.noreply.github.com>
2025-03-25 20:43:49 +0530

    Time: 562 ms (70.47%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,10 +1,4 @@
-# Write your MySQL query statement below
-
-SELECT e1.id,
-(CASE 
-WHEN MOD(e1.id,2)=0 THEN COALESCE(e2.student,e1.student) 
-ELSE COALESCE(LEAD(e1.student,1) OVER(ORDER BY e1.id),e1.student)
-END) as student
-FROM Seat as e1 LEFT JOIN Seat as e2
-ON e1.id - 1 = e2.id
-ORDER BY e1.id;
+SELECT 
+    id, 
+    IF(MOD(id, 2) = 1, LEAD(student, 1, student) OVER (), LAG(student,1) OVER ()) AS student
+FROM Seat;
